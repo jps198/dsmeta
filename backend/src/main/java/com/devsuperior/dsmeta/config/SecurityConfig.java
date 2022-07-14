0c46348a97d0982146327d7a3434760d28e82302
João Paulo dos Santos <joao_pls84@yahoo.com.br>
2022-07-14 11:50:41 -0300

    Twilio SMS

--- backend/src/main/java/com/devsuperior/dsmeta/config/SecurityConfig.java
+++ backend/src/main/java/com/devsuperior/dsmeta/config/SecurityConfig.java
@@ -10,23 +10,25 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-@Bean
-public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-http.headers().frameOptions().disable();
-http.cors().and().csrf().disable();
-http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-http.authorizeHttpRequests((auth) -> auth.anyRequest().permitAll());
-return http.build();
-} @
-Bean
-CorsConfigurationSource corsConfigurationSource() {
-CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
-configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
-final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-source.registerCorsConfiguration("/**", configuration);
-return source;
-}
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.headers().frameOptions().disable();
+		http.cors().and().csrf().disable();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.authorizeHttpRequests((auth) -> auth.anyRequest().permitAll());
+		return http.build();
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+		configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
 }
