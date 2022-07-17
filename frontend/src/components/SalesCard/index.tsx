17bab41908b77463f897b7b1c238fb8d45c2433d
João Paulo dos Santos <joao_pls84@yahoo.com.br>
2022-07-17 15:32:44 -0300

    Date Update

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -19,12 +19,16 @@ function SalesCard() {
 
   // Buscar do back end as nossas vendas e guardou dentro da lista sales
   useEffect(() => {
+    const dmin = minDate.toISOString().slice(0, 10);
+    const dmax = maxDate.toISOString().slice(0, 10);
+    //console.log (dmin);
+
     /*console.log("TESTE");*/
-    axios.get(`${BASE_URL}/sales`).then((response) => {
+    axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`).then((response) => {
       // console.log(response.data);
       setSales(response.data.content);
     });
-  }, []);
+  }, [minDate, maxDate]);
 
   return (
     <div className="card">
@@ -65,7 +69,9 @@ function SalesCard() {
               return (
                 <tr key={sale.id}>
                   <td className="show992">{sale.id}</td>
-                  <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
+                  <td className="show576">
+                    {new Date(sale.date).toLocaleDateString()}
+                  </td>
                   <td>{sale.sellerName}</td>
                   <td className="show992">{sale.visited}</td>
                   <td className="show992">{sale.deals}</td>
